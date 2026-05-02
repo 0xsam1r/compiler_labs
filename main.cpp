48a71dbfe9d6c337496b75bba7776674454ebfcb
0xsam1r <146773599+0xsam1r@users.noreply.github.com>
2026-05-02 23:34:37 +0300

    lec3-full-implementation-for-grammer

--- main.cpp
+++ main.cpp
@@ -1,6 +1,50 @@
+#include <cctype>
+#include <cstdio>
 #include <iostream>
-void term() {std::cout<<"this is a term\n";}
-void rest() {std::cout<<"this is a rest of code\n";}
+
+using namespace std;
+
+int lookahead = getchar();
+
+void error() {
+	cout << "Syntax Error\n";
+	exit(1);
+}
+
+void match(int t) {
+	if (lookahead == t )
+		lookahead = getchar();
+	else
+		error();
+}
+
+void term() {
+	if(isdigit(lookahead))
+	{
+		putchar(lookahead);
+		match(lookahead);
+	}
+	else 
+		error();
+
+}
+
+void rest() {
+	while (true) {
+		if (lookahead == '+') {
+			match(lookahead);
+			term();
+			putchar(lookahead);
+		}
+		else if (lookahead == '-') {
+			match(lookahead);
+			term();
+			putchar(lookahead);
+		}
+		else 
+			break;
+	}
+}
 
 void expr(){
 	term();
@@ -8,5 +52,6 @@ void expr(){
 }
 
 int main() {
+	cout<<"Start Parsing now...\n";
 	expr();
 }
